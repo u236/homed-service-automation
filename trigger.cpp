a7c8ae4149097c37b9392d8431e427a9516cc813
u236 <mail@u236.org>
2024-12-09 12:44:08 +0300

    little update

--- trigger.cpp
+++ trigger.cpp
@@ -5,20 +5,20 @@ bool TriggerObject::match(const QVariant &oldValue, const QVariant &newValue, St
     switch (statement)
     {
         case Statement::equals:  return oldValue != value && newValue == value;
-        case Statement::above:   return oldValue.toDouble() < value.toDouble() && newValue.toDouble() >= value.toDouble();
-        case Statement::below:   return oldValue.toDouble() > value.toDouble() && newValue.toDouble() <= value.toDouble();
+        case Statement::above:   return (!oldValue.isValid() || oldValue.toDouble() < value.toDouble()) && newValue.toDouble() >= value.toDouble();
+        case Statement::below:   return (!oldValue.isValid() || oldValue.toDouble() > value.toDouble()) && newValue.toDouble() <= value.toDouble();
 
         case Statement::between:
         {
             QList <QVariant> list = value.toList();
             double a = oldValue.toDouble(), b = newValue.toDouble(), min = qMin(list.value(0).toDouble(), list.value(1).toDouble()), max = qMax(list.value(0).toDouble(), list.value(1).toDouble());
-            return (a < min || a > max) && b >= min && b <= max;
+            return (!oldValue.isValid() || a < min || a > max) && b >= min && b <= max;
         }
 
         case Statement::changes:
         {
             double a = oldValue.toDouble(), b = newValue.toDouble(), change = value.toDouble();
-            return b != a && (b <= a - change || b >= a + change);
+            return a != b && (b <= a - change || b >= a + change);
         }
 
         case Statement::updates: return oldValue != newValue;
