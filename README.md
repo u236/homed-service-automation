6a50eda7dc549fa2ba20b84d73464caa45fe0198
u236 <mail@u236.org>
2025-10-23 17:56:21 +0300

    fix readme wiki link

--- README.md
+++ README.md
@@ -6,4 +6,4 @@
 https://github.com/u236/homed-service-common
 
 Документация:\
-https://wiki.homed.dev/page/Automation
+https://wiki.homed.dev/automation/
--- controller.h
+++ controller.h
@@ -1,7 +1,7 @@
 #ifndef CONTROLLER_H
 #define CONTROLLER_H
 
-#define SERVICE_VERSION         "2.2.5"
+#define SERVICE_VERSION         "2.3.0"
 #define EMPTY_PATTERN_VALUE     "_NULL_"
 #define SUBSCRIPTION_DELAY      1000
 #define RUNNER_STARTUP_DELAY    10
