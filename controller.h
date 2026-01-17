31ffeca23108b7bd9a2a9752aa96ff7a11c80b3f
u236 <mail@u236.org>
2026-01-17 16:14:29 +0300

    fix telegram unicode bug

--- controller.h
+++ controller.h
@@ -1,7 +1,7 @@
 #ifndef CONTROLLER_H
 #define CONTROLLER_H
 
-#define SERVICE_VERSION         "2.3.3"
+#define SERVICE_VERSION         "2.3.4"
 #define EMPTY_PATTERN_VALUE     "_NULL_"
 #define SUBSCRIPTION_DELAY      1000
 #define RUNNER_STARTUP_DELAY    10
